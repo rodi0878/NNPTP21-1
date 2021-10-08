1c30fb23c7449ba74f914f85e156b21f90e406ef
koprivovamonika <72449295+koprivovamonika@users.noreply.github.com>
2021-10-08 21:33:19 +0200

    New classes

---
+++ INPTPZ1/ComplexNumber.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace INPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class ComplexNumber
+        {
+            public double Re { get; set; }
+            public double Im { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is ComplexNumber)
+                {
+                    ComplexNumber complexNumber = obj as ComplexNumber;
+                    return complexNumber.Re == Re && complexNumber.Im == Im;
+                }
+                return base.Equals(obj);
+            }
+
+            public readonly static ComplexNumber Zero = new ComplexNumber()
+            {
+                Re = 0,
+                Im = 0
+            };
+            public ComplexNumber Add(ComplexNumber secondNumber)
+            {
+                return new ComplexNumber()
+                {
+                    Re = Re + secondNumber.Re,
+                    Im = Im + secondNumber.Im
+                };
+            }
+
+            public ComplexNumber Multiply(ComplexNumber secondNumber)
+            {
+                return new ComplexNumber()
+                {
+                    Re = Re * secondNumber.Re - Im * secondNumber.Im,
+                    Im = (float)(Re * secondNumber.Im + Im * secondNumber.Re)
+                };
+            }
+
+            public ComplexNumber Subtract(ComplexNumber secondNumber)
+            {
+                return new ComplexNumber()
+                {
+                    Re = Re - secondNumber.Re,
+                    Im = Im - secondNumber.Im
+                };
+            }
+
+            public ComplexNumber Divide(ComplexNumber secondNumber)
+            {
+                ComplexNumber dividedNumber = Multiply(new ComplexNumber() { Re = secondNumber.Re, Im = -secondNumber.Im });
+                double divisor = Math.Pow(secondNumber.Re, 2) + Math.Pow(secondNumber.Im, 2);
+
+                return new ComplexNumber()
+                {
+                    Re = dividedNumber.Re / divisor,
+                    Im = (float)(dividedNumber.Im / divisor)
+                };
+            }
+            public double GetAbsoluteValue()
+            {
+                return Math.Sqrt( Math.Pow(Re,2) + Math.Pow(Im, 2));
+            }
+
+            public double GetAngle()
+            {
+                return Math.Atan(Im / Re);
+            }
+
+            public override string ToString()
+            {
+                return $"({Re} + {Im}i)";
+            }
+
+            /*
+             * TODO: zjistit jestli ano nebo ne
+             * 
+             * public override int GetHashCode()
+            {
+                var hashCode = 29377563;
+                hashCode = hashCode * -1521134295 + Re.GetHashCode();
+                hashCode = hashCode * -1521134295 + Im.GetHashCode();
+                return hashCode;
+            }*/
+        }
+    }
+}
---
+++ INPTPZ1/Polynome.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+
+namespace INPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class Polynome
+        {
+            public List<ComplexNumber> Coefficients { get; set; }
+
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            public Polynome()
+            {
+                Coefficients = new List<ComplexNumber>();
+            }
+
+            public void Add(ComplexNumber coeficient)
+            {
+                Coefficients.Add(coeficient);
+            }
+
+            /// <summary>
+            /// Derives this polynomial and creates new one
+            /// </summary>
+            /// <returns>Derivated polynomial</returns>
+            public Polynome Derive()
+            {
+                Polynome polynome = new Polynome();
+                for (int i = 1; i < Coefficients.Count; i++)
+                {
+                    polynome.Coefficients.Add(Coefficients[i].Multiply(new ComplexNumber() { Re = i }));
+                }
+
+                return polynome;
+            }
+
+            /// <summary>
+            /// Evaluates polynomial at given point
+            /// </summary>
+            /// <param name="realPartOfEvaluationPoint">point of evaluation</param>
+            /// <returns>y</returns>
+            public ComplexNumber Evaluation(double realPartOfEvaluationPoint)
+            {
+                return Evaluation(new ComplexNumber() { Re = realPartOfEvaluationPoint, Im = 0 });
+            }
+
+            /// <summary>
+            /// Evaluates polynomial at given point
+            /// </summary>
+            /// <param name="pointOfEvaluation">point of evaluation</param>
+            /// <returns>y</returns>
+            public ComplexNumber Evaluation(ComplexNumber pointOfEvaluation)
+            {
+                ComplexNumber resultPolynomial = ComplexNumber.Zero;
+                for (int i = 0; i < Coefficients.Count; i++)
+                {
+                    ComplexNumber coefficient = Coefficients[i];
+                    ComplexNumber copiedPointOfEvaluation = pointOfEvaluation;
+
+                    if (i > 0)
+                    {
+                        for (int j = 0; j < i - 1; j++)
+                        {
+                            copiedPointOfEvaluation = copiedPointOfEvaluation.Multiply(pointOfEvaluation);
+                        }
+                        coefficient = coefficient.Multiply(copiedPointOfEvaluation);
+                    }
+
+                    resultPolynomial = resultPolynomial.Add(coefficient);
+                }
+
+                return resultPolynomial;
+            }
+
+            /// <summary>
+            /// ToString
+            /// </summary>
+            /// <returns>String repr of polynomial</returns>
+            public override string ToString()
+            {
+                string resultPolynomial = "";
+                for (int i = 0; i < Coefficients.Count; i++)
+                {
+                    resultPolynomial += Coefficients[i];
+                    if (i > 0)
+                    {
+                        for (int j = 0 ; j < i; j++)
+                        {
+                            resultPolynomial += "x";
+                        }
+                    }
+                    if (i + 1 < Coefficients.Count)
+                    {
+                        resultPolynomial += " + ";
+                    }
+                    
+                }
+                return resultPolynomial;
+            }
+        }
+    }
+}
