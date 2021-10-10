13b5af1724b97b929a52987fa71bf6b8aee0414b
Tomas Prudky <tomas-o@seznam.cz>
2021-10-10 20:30:33 +0200

    Basic modifications. Rename variables and create classes.

---
+++ INPTPZ1/ComplexNumber.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Mathematics
+{
+    public class ComplexNumber
+    {
+        public double RealPart { get; set; }
+        public double ImaginaryPart { get; set; }
+
+        public readonly static ComplexNumber Zero = new ComplexNumber()
+        {
+            RealPart = 0,
+            ImaginaryPart = 0
+        };
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ComplexNumber)
+            {
+                ComplexNumber x = obj as ComplexNumber;
+                return x.RealPart == RealPart && x.ImaginaryPart == ImaginaryPart;
+            }
+            return base.Equals(obj);
+        }
+
+        public ComplexNumber Multiply(ComplexNumber b)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = this.RealPart * b.RealPart - this.ImaginaryPart * b.ImaginaryPart,
+                ImaginaryPart = this.RealPart * b.ImaginaryPart + this.ImaginaryPart * b.RealPart
+            };
+        }
+
+        public ComplexNumber Divide(ComplexNumber b)
+        {
+            ComplexNumber tmp = this.Multiply(new ComplexNumber() { RealPart = b.RealPart, ImaginaryPart = -b.ImaginaryPart });
+            double tmp2 = b.RealPart * b.RealPart + b.ImaginaryPart * b.ImaginaryPart;
+
+            return new ComplexNumber()
+            {
+                RealPart = tmp.RealPart / tmp2,
+                ImaginaryPart = tmp.ImaginaryPart / tmp2
+            };
+        }
+
+        public double GetAbs()
+        {
+            return Math.Sqrt(RealPart * RealPart + ImaginaryPart * ImaginaryPart);
+        }
+
+        public ComplexNumber Add(ComplexNumber b)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = this.RealPart + b.RealPart,
+                ImaginaryPart = this.ImaginaryPart + b.ImaginaryPart
+            };
+        }
+
+        public ComplexNumber Subtract(ComplexNumber b)
+        {
+            return new ComplexNumber()
+            {
+                RealPart = this.RealPart - b.RealPart,
+                ImaginaryPart = this.ImaginaryPart - b.ImaginaryPart
+            };
+        }
+
+        public double GetAngle()
+        {
+            return Math.Atan(ImaginaryPart / RealPart);
+        }
+
+        public override string ToString()
+        {
+            return $"({RealPart} + {ImaginaryPart}i)";
+        }
+
+    }
+}
---
+++ INPTPZ1/Polynomial.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace Mathematics
+{
+    public class Polynomial
+    {
+        public List<ComplexNumber> CompleNumbersList { get; set; }
+
+        public Polynomial()
+        {
+            CompleNumbersList = new List<ComplexNumber>();
+        }
+
+        public void Add(ComplexNumber complexNumber)
+        {
+            CompleNumbersList.Add(complexNumber);
+        }
+
+        public Polynomial Derive()
+        {
+            Polynomial polynomial = new Polynomial();
+            for (int i = 1; i < CompleNumbersList.Count; i++)
+            {
+                polynomial.CompleNumbersList.Add(CompleNumbersList[i].Multiply(new ComplexNumber() { RealPart = i }));
+            }
+
+            return polynomial;
+        }
+
+        public ComplexNumber Evaluate(double x)
+        {
+            ComplexNumber result = Evaluate(new ComplexNumber() { RealPart = x, ImaginaryPart = 0 });
+            return result;
+        }
+
+        public ComplexNumber Evaluate(ComplexNumber x)
+        {
+            ComplexNumber complexNumber = ComplexNumber.Zero;
+            for (int i = 0; i < CompleNumbersList.Count; i++)
+            {
+                ComplexNumber coefficient = CompleNumbersList[i];
+                ComplexNumber bx = x;
+                int power = i;
+
+                if (i > 0)
+                {
+                    for (int j = 0; j < power - 1; j++)
+                        bx = bx.Multiply(x);
+
+                    coefficient = coefficient.Multiply(bx);
+                }
+
+                complexNumber = complexNumber.Add(coefficient);
+            }
+
+            return complexNumber;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < CompleNumbersList.Count; i++)
+            {
+                s += CompleNumbersList[i];
+                if (i > 0)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        s += "x";
+                    }
+                }
+                if (i + 1 < CompleNumbersList.Count)
+                    s += " + ";
+            }
+            return s;
+        }
+    }
+}
+
