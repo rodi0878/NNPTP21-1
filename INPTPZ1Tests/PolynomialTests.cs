27f025d0ac4036f4bdf64c2c2008cbb6f81a9929
LLetacek <72096612+LLetacek@users.noreply.github.com>
2021-10-11 11:26:37 +0200

    renaming variables in tests

--- INPTPZ1Tests/PolynomialTests.cs
+++ INPTPZ1Tests/PolynomialTests.cs
@@ -7,53 +7,53 @@ namespace INPTPZ1.Mathematics.Tests
     {
 
         [TestMethod()]
-        public void PolynomeEvaluateTestWithComplexNumber0Re0Im()
+        public void PolynomialEvaluateTestWithComplexNumber0Re0Im()
         {
-            Polynomial polynome = new Polynomial();
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 0, Im = 0 });
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
+            Polynomial polynomial = new Polynomial();
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 0, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
 
-            ComplexNumber result = polynome.Evaluate(new ComplexNumber() { Re = 0, Im = 0 });
+            ComplexNumber result = polynomial.Evaluate(new ComplexNumber() { Re = 0, Im = 0 });
             ComplexNumber expected = new ComplexNumber() { Re = 1, Im = 0 };
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod()]
-        public void PolynomeEvaluateTestWithComplexNumber1Re0Im()
+        public void PolynomialEvaluateTestWithComplexNumber1Re0Im()
         {
-            Polynomial polynome = new Polynomial();
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 0, Im = 0 });
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
+            Polynomial polynomial = new Polynomial();
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 0, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
 
-            ComplexNumber result = polynome.Evaluate(new ComplexNumber() { Re = 1, Im = 0 });
+            ComplexNumber result = polynomial.Evaluate(new ComplexNumber() { Re = 1, Im = 0 });
             ComplexNumber expected = new ComplexNumber() { Re = 2, Im = 0 };
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod()]
-        public void PolynomeEvaluateTestWithComplexNumber2Re0Im()
+        public void PolynomialEvaluateTestWithComplexNumber2Re0Im()
         {
-            Polynomial polynome = new Polynomial();
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 0, Im = 0 });
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
+            Polynomial polynomial = new Polynomial();
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 0, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
 
-            ComplexNumber result = polynome.Evaluate(new ComplexNumber() { Re = 2, Im = 0 });
+            ComplexNumber result = polynomial.Evaluate(new ComplexNumber() { Re = 2, Im = 0 });
             ComplexNumber expected = new ComplexNumber() { Re = 5.0000000000, Im = 0 };
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod()]
-        public void PolynomeToStringTest()
+        public void PolynomialToStringTest()
         {
-            Polynomial polynome = new Polynomial();
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 0, Im = 0 });
-            polynome.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
+            Polynomial polynomial = new Polynomial();
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 0, Im = 0 });
+            polynomial.Coefficients.Add(new ComplexNumber() { Re = 1, Im = 0 });
 
-            string result = polynome.ToString();
+            string result = polynomial.ToString();
             string expected = "(1 + 0i) + (0 + 0i)x + (1 + 0i)xx";
             Assert.AreEqual(expected, result);
         }
