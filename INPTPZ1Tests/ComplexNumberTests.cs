9cc41276bab900905a50dc9c31ebcbf1cbb141d7
David Le <61124841+david-d-le@users.noreply.github.com>
2021-10-11 09:37:40 +0200

    refactoring of test methods

---
+++ INPTPZ1Tests/ComplexNumberTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace INPTPZ1.Mathematics.Tests
+{
+    [TestClass()]
+    public class ComplexNumberTests
+    {
+
+        [TestMethod()]
+        public void AddTest1()
+        {
+            ComplexNumber augend = new ComplexNumber() { Re = 10, Im = 20 };
+            ComplexNumber addend = new ComplexNumber() { Re = 1, Im = 2 };
+            ComplexNumber expected = new ComplexNumber() { Re = 11, Im = 22 };
+            ComplexNumber result = augend.Add(addend);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod()]
+        public void AddTest2()
+        {
+            ComplexNumber augend = new ComplexNumber() { Re = 1, Im = -1 };
+            ComplexNumber addend = new ComplexNumber() { Re = 0, Im = 0 };
+            ComplexNumber expected = new ComplexNumber() { Re = 1, Im = -1 };
+            ComplexNumber result = augend.Add(addend);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToStringTest1()
+        {
+            ComplexNumber complexNumber = new ComplexNumber() { Re = 10, Im = 20 };
+            string expected = "(10 + 20i)";
+            string result = complexNumber.ToString();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToStringTest2()
+        {
+            ComplexNumber complexNumber = new ComplexNumber() { Re = 1, Im = 2 };
+            string expected = "(1 + 2i)";
+            string result = complexNumber.ToString();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToStringTest3()
+        {
+            ComplexNumber complexNumber = new ComplexNumber() { Re = 1, Im = -1 };
+            string expected = "(1 + -1i)";
+            string result = complexNumber.ToString();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToStringTest4()
+        {
+            ComplexNumber complexNumber = new ComplexNumber() { Re = 0, Im = 0 };
+            string expected = "(0 + 0i)";
+            string result = complexNumber.ToString();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
+
---
+++ INPTPZ1Tests/PolynomialTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace INPTPZ1.Mathematics.Tests
+{
+    /// <summary>
+    /// Souhrnný popis pro PolynomialTests
+    /// </summary>
+    [TestClass]
+    public class PolynomialTests
+    {
+        [TestMethod()]
+        public void EvaluateAtPointXTest1()
+        {
+            Polynomial polynomial = new Polynomial(new ComplexNumber() { Re = 1, Im = 0 }, new ComplexNumber() { Re = 0, Im = 0 }, new ComplexNumber() { Re = 1, Im = 0 });
+            ComplexNumber expected = new ComplexNumber() { Re = 1, Im = 0 };
+            ComplexNumber result = polynomial.EvaluateAtPointX(new ComplexNumber() { Re = 0, Im = 0 });
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void EvaluateAtPointXTest2()
+        {
+            Polynomial polynomial = new Polynomial(new ComplexNumber() { Re = 1, Im = 0 }, new ComplexNumber() { Re = 0, Im = 0 }, new ComplexNumber() { Re = 1, Im = 0 });
+            ComplexNumber result = polynomial.EvaluateAtPointX(new ComplexNumber() { Re = 1, Im = 0 });
+            ComplexNumber expected = new ComplexNumber() { Re = 2, Im = 0 };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void EvaluateAtPointXTest3()
+        {
+            Polynomial polynomial = new Polynomial(new ComplexNumber() { Re = 1, Im = 0 }, new ComplexNumber() { Re = 0, Im = 0 }, new ComplexNumber() { Re = 1, Im = 0 });
+            ComplexNumber result = polynomial.EvaluateAtPointX(new ComplexNumber() { Re = 2, Im = 0 });
+            ComplexNumber expected = new ComplexNumber() { Re = 5.0000000000, Im = 0 };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToStringTest1()
+        {
+            Polynomial polynomial = new Polynomial(new ComplexNumber() { Re = 1, Im = 0 }, new ComplexNumber() { Re = 0, Im = 0 }, new ComplexNumber() { Re = 1, Im = 0 });
+            string result = polynomial.ToString();
+            string expected = "(1 + 0i) + (0 + 0i)x + (1 + 0i)xx";
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
