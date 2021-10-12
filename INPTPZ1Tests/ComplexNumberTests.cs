8d389c485e1890dcf9cd3017b6f46eea42b10d4b
lev-michael <michael.lev@hrsd.cz>
2021-10-12 17:42:02 +0200

    Refactor tests

---
+++ INPTPZ1Tests/ComplexNumberTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace INPTPZ1.Mathematics.Tests
+{
+    [TestClass()]
+    public class ComplexNumberTests
+    {
+        public ComplexNumber ComplexNumber { get; set; }
+
+        [TestInitialize]
+        public void TestInitializePolynome()
+        {
+            ComplexNumber = new ComplexNumber()
+            {
+                Real = 10,
+                Imaginary = 20
+            };  
+        }
+
+        [TestMethod()]
+        public void TestComplexNumberAddition()
+        {
+            ComplexNumber result = ComplexNumber.Add(new ComplexNumber() { Real = 1, Imaginary = 2 });
+            ComplexNumber expected = new ComplexNumber()
+            {
+                Real = 11,
+                Imaginary = 22
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void TestComplexNumberSubstraction()
+        {
+            ComplexNumber result = ComplexNumber.Subtract(new ComplexNumber() { Real = 1, Imaginary = 2 });
+            ComplexNumber expected = new ComplexNumber()
+            {
+                Real = 9,
+                Imaginary = 18
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void TestComplexNumberMultiplication()
+        {
+            ComplexNumber result = ComplexNumber.Multiply(new ComplexNumber() { Real = 5, Imaginary = 2 });
+            ComplexNumber expected = new ComplexNumber()
+            {
+                Real = 10,
+                Imaginary = 120
+            };
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void TestComplexNumberAbs()
+        {
+            double result = ComplexNumber.GetAbs();
+            double expected = Math.Sqrt(Math.Pow(10, 2) + Math.Pow(20, 2));
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void TestComplexNumberAngleInRadians()
+        {
+            double result = ComplexNumber.GetAngleInRadians();
+            Assert.AreEqual(Math.Atan(20 / 10), result);
+        }
+
+        [TestMethod()]
+        public void TestComplexNumberToString()
+        {
+            String result = ComplexNumber.ToString();
+            Assert.AreEqual("(10 + 20i)", result);
+        }
+
+        [TestMethod()]
+        public void TestComplexNumberZero()
+        {
+            ComplexNumber result = ComplexNumber.Zero;
+            ComplexNumber expected = new ComplexNumber()
+            {
+                Real = 0,
+                Imaginary = 0
+            };
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
+
---
+++ INPTPZ1Tests/PolynomeTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace INPTPZ1.Mathematics.Tests
+{
+    [TestClass()]
+    public class PolynomeTests
+    {
+        public Polynome Polynome { get; set; }
+
+        [TestInitialize]
+        public void TestInitializePolynome()
+        {
+            Polynome = new Polynome()
+            {
+                Coefficients =
+                {
+                    new ComplexNumber() { Real = 1, Imaginary = 0 },
+                    ComplexNumber.Zero,
+                    new ComplexNumber() { Real = 1, Imaginary = 0 },
+
+                }
+            };
+        }
+
+        [TestMethod()]
+        public void TestPolynomeEvaluationComplexNumberZero()
+        {
+            ComplexNumber result = Polynome.Evaluate(new ComplexNumber() { Real = 0, Imaginary = 0 });
+            ComplexNumber expected = new ComplexNumber() { Real = 1, Imaginary = 0 };
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void TestPolynomeEvaluationComplexNumberReal1Imaginary0()
+        {
+            ComplexNumber result = Polynome.Evaluate(new ComplexNumber() { Real = 1, Imaginary = 0 });
+            ComplexNumber expected = new ComplexNumber() { Real = 2, Imaginary = 0 };
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void TestPolynomeEvaluationComplexNumberReal2Imaginary0()
+        {
+            ComplexNumber result = Polynome.Evaluate(new ComplexNumber() { Real = 2, Imaginary = 0 });
+            ComplexNumber expected = new ComplexNumber() { Real = 5, Imaginary = 0 };
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void TestPolynomeToString()
+        {
+            string result = Polynome.ToString();
+            string expected = "(1 + 0i) + (0 + 0i)x + (1 + 0i)xx";
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
+
