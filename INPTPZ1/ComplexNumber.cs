566d0986a115ae639cb8bbcd812c3a0b7c085f02
lev-michael <michael.lev@hrsd.cz>
2021-10-12 19:36:23 +0200

    add classes for complex number and polynome

---
+++ INPTPZ1/ComplexNumber.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace INPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class ComplexNumber
+        {
+            public double Real { get; set; }
+            public double Imaginary { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is ComplexNumber)
+                {
+                    ComplexNumber complexNumber = obj as ComplexNumber;
+                    return complexNumber.Real == Real && complexNumber.Imaginary == Imaginary;
+                }
+                return base.Equals(obj);
+            }
+
+            public readonly static ComplexNumber Zero = new ComplexNumber()
+            {
+                Real = 0,
+                Imaginary = 0
+            };
+
+            public ComplexNumber Add(ComplexNumber complexNumberToAdd)
+            {
+                return new ComplexNumber()
+                {
+                    Real = this.Real + complexNumberToAdd.Real,
+                    Imaginary = this.Imaginary + complexNumberToAdd.Imaginary
+                };
+            }
+
+            public ComplexNumber Subtract(ComplexNumber complexNumberToSubstract)
+            {
+                return new ComplexNumber()
+                {
+                    Real = this.Real - complexNumberToSubstract.Real,
+                    Imaginary = this.Imaginary - complexNumberToSubstract.Imaginary
+                };
+            }
+
+            public ComplexNumber Multiply(ComplexNumber complexNumberToMultiply)
+            {
+                return new ComplexNumber()
+                {
+                    Real = this.Real * complexNumberToMultiply.Real - this.Imaginary * complexNumberToMultiply.Imaginary,
+                    Imaginary = (this.Real * complexNumberToMultiply.Imaginary + this.Imaginary * complexNumberToMultiply.Real)
+                };
+            }
+
+            internal ComplexNumber Divide(ComplexNumber complexNumberToDivide)
+            {
+                var dividedNumber = this.Multiply(new ComplexNumber() { Real = complexNumberToDivide.Real, Imaginary = -complexNumberToDivide.Imaginary });
+                var divisor = complexNumberToDivide.Real * complexNumberToDivide.Real + complexNumberToDivide.Imaginary * complexNumberToDivide.Imaginary;
+
+                return new ComplexNumber()
+                {
+                    Real = dividedNumber.Real / divisor,
+                    Imaginary = dividedNumber.Imaginary / divisor
+                };
+            }
+
+            public double GetAbs()
+            {
+                return Math.Sqrt(Real * Real + Imaginary * Imaginary);
+            }
+
+            public double GetAngleInRadians()
+            {
+                return Math.Atan(Imaginary / Real);
+            }
+
+            public override string ToString()
+            {
+                return $"({Real} + {Imaginary}i)";
+            }
+
+            public override int GetHashCode()
+            {
+                var hashCode = -837395861;
+                hashCode = hashCode * -1521134295 + Real.GetHashCode();
+                hashCode = hashCode * -1521134295 + Imaginary.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ INPTPZ1/Polynome.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace INPTPZ1
+{
+
+    namespace Mathematics
+    {
+        public class Polynome
+        {
+            public List<ComplexNumber> Coefficients { get; set; }
+
+            public Polynome()
+            {
+                Coefficients = new List<ComplexNumber>();
+            }
+
+            public void Add(ComplexNumber complexNumber)
+            {
+                Coefficients.Add(complexNumber);
+            }
+
+
+            public Polynome Derive()
+            {
+                Polynome polynome = new Polynome();
+                for (int i = 1; i < Coefficients.Count; i++)
+                {
+                    polynome.Coefficients.Add(Coefficients[i].Multiply(new ComplexNumber() { Real = i }));
+                }
+                return polynome;
+            }
+
+            public ComplexNumber Evaluate(double realNumberPointOfEvaluation)
+            {
+                return Evaluate(new ComplexNumber() { Real = realNumberPointOfEvaluation, Imaginary = 0 });
+            }
+
+            public ComplexNumber Evaluate(ComplexNumber pointOfEvaluation)
+            {
+                ComplexNumber result = ComplexNumber.Zero;
+                for (int i = 0; i < Coefficients.Count; i++)
+                {
+                    ComplexNumber coefficient = Coefficients[i];
+                    if (i > 0)
+                    {
+                        for (int j = 0; j < i - 1; j++)
+                        {
+                            pointOfEvaluation = pointOfEvaluation.Multiply(pointOfEvaluation);
+                        }
+
+                        coefficient = coefficient.Multiply(pointOfEvaluation);
+                    }
+                    result = result.Add(coefficient);
+                }
+                return result;
+            }
+
+            public override string ToString()
+            {
+                string resultPolynomeString = "";
+                for (int i = 0; i < Coefficients.Count; i++)
+                {
+                    resultPolynomeString += Coefficients[i];
+                    if (i > 0)
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            resultPolynomeString += "x";
+                        }
+                    }
+                    if (i + 1 < Coefficients.Count)
+                    {
+                        resultPolynomeString += " + ";
+                    }
+                }
+                return resultPolynomeString;
+            }
+        }
+    }
+}
