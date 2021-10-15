3e9aec4e6820290749ae80312901b81aff334e61
st55790 <tomas-o@seznam.cz>
2021-10-15 14:54:50 +0200

    Refactoring tests

---
+++ INPTPZ1Tests/ComplexNumberTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathematics;
+
+namespace INPTPZ1.Mathematics.Tests
+{
+    [TestClass()]
+    public class ComplexNumberTests
+    {
+        private ComplexNumber firstNumber;
+        private ComplexNumber secondNumber;
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            firstNumber = new ComplexNumber()
+            {
+                RealPart = 10,
+                ImaginaryPart = 20
+            };
+
+            secondNumber = new ComplexNumber()
+            {
+                RealPart = 1,
+                ImaginaryPart = 2
+            };
+        }
+
+        [TestMethod()]
+        public void ComplexNumberTestAdd()
+        {
+            ComplexNumber actual = firstNumber.Add(secondNumber);
+            ComplexNumber expected = new ComplexNumber()
+            {
+                RealPart = 11,
+                ImaginaryPart = 22
+            };
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberTestToStringFirstNumber()
+        {
+            string expected = "(10 + 20i)";
+            string result = firstNumber.ToString();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberToStringSecondNumber() 
+        {
+            string expected = "(1 + 2i)";
+            string result = secondNumber.ToString();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberAddWithDifferentValues() {
+            firstNumber.RealPart = 1;
+            firstNumber.ImaginaryPart = -1;
+
+            secondNumber.RealPart = 0; 
+            secondNumber.ImaginaryPart = 0;
+
+            ComplexNumber expected = new ComplexNumber() 
+            { 
+                RealPart = 1, 
+                ImaginaryPart = -1 
+            };
+
+            ComplexNumber result = firstNumber.Add(secondNumber);       
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberTestToStringWithDifferentValues()
+        {
+            firstNumber.RealPart = 1;
+            firstNumber.ImaginaryPart = -1;
+            
+            string expected = "(1 + -1i)";
+            string result = firstNumber.ToString();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ComplexNumberTestToStringWithZeroes()
+        {
+            secondNumber.RealPart = 0;
+            secondNumber.ImaginaryPart = 0;
+
+            string expected = "(0 + 0i)";
+            string result = secondNumber.ToString();
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
+
+
---
+++ INPTPZ1Tests/PolynomialTests.cs
@@ -0,0 +1,83 @@
+﻿using Mathematics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace INPTPZ1Tests
+{
+    [TestClass]
+    public class PolynomialTests
+    {
+        private Polynomial polynomial;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            polynomial = new Polynomial();
+            polynomial.ComplexNumbersList.Add(new ComplexNumber() { RealPart = 1, ImaginaryPart = 0 });
+            polynomial.ComplexNumbersList.Add(new ComplexNumber() { RealPart = 0, ImaginaryPart = 0 });
+            polynomial.ComplexNumbersList.Add(new ComplexNumber() { RealPart = 1, ImaginaryPart = 0 });
+        }
+
+        [TestMethod()]
+        public void PolynomialTestEvalueteComplexNumberVersion1()
+        {
+            ComplexNumber result = polynomial.Evaluate(new ComplexNumber() 
+            { 
+                RealPart = 0, 
+                ImaginaryPart = 0 
+            });
+
+            ComplexNumber expected = new ComplexNumber() 
+            { 
+                RealPart = 1, 
+                ImaginaryPart = 0 
+            };
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod()]
+        public void PolynomialTestEvalueteComplexNumberVersion2()
+        {
+            ComplexNumber result = polynomial.Evaluate(new ComplexNumber() 
+            { 
+                RealPart = 1, 
+                ImaginaryPart = 0 
+            });
+
+            ComplexNumber expected = new ComplexNumber() 
+            { 
+                RealPart = 2, 
+                ImaginaryPart = 0 
+            };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void PolynomialTestEvalueteComplexNumberVersion3()
+        {
+            ComplexNumber result = polynomial.Evaluate(new ComplexNumber() 
+            { 
+                RealPart = 2, 
+                ImaginaryPart = 0 
+            });
+
+            ComplexNumber expected = new ComplexNumber() 
+            { 
+                RealPart = 5.0000000000, 
+                ImaginaryPart = 0 
+            };
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void PolynomialTestToString()
+        {
+            string result = polynomial.ToString();
+            string expected = "(1 + 0i) + (0 + 0i)x + (1 + 0i)xx";
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
